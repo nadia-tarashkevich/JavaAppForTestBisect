b1de289b75b48e43ed7e8e712ca8961d666d86bb
nadia <nadia.tarashkevich@jetbrains.com>
2023-04-26 18:10:23 -0700

    Initial commit

---
+++ src/ShowRectangle.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ShowRectangle extends JFrame {
+
+    public ShowRectangle() {
+        setTitle("Green Rectangle");
+        setSize(200, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.setColor(Color.GREEN);
+                g.fillRect(50, 50, 100, 100);
+            }
+        };
+
+        add(panel);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new ShowRectangle();
+    }
+}
